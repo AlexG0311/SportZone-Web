33aa66876ed70ab69744a30b3cb3cad0eee91363
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2026-01-18 14:25:22 -0500

    se agrego el type Session

--- src/App/hooks/ServicioClientSupabase.ts
+++ src/App/hooks/ServicioClientSupabase.ts
@@ -1,11 +1,12 @@
 
 import { useEffect, useState } from "react";
 import { supabaseCliente  } from "../pages/supabase/clienteSupabase.ts";
+import { Session } from '@supabase/supabase-js';
 
 
 
 export const ServicioClientSupabase = () => {
-const [session, setSession] = useState<any>(null);
+const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
    // Check for existing session
