da4c52c1846301bd651efe18395c6052433181a4
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2026-02-09 23:38:27 -0500

    main optimizado

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import { createRoot } from 'react-dom/client'
-
-import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './App/provider/AuthProvider.tsx';
 import './index.css'
 import App from './App.tsx'
 
-
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
     <AuthProvider>
         <App />
     </AuthProvider>
-  </BrowserRouter>,
 )
 
 
